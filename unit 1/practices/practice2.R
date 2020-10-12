799a51820f1e662eec02c3196aafbe57b920d961
IrvingQuirozV <71294033+IrvingQuirozV@users.noreply.github.com>
2020-10-11 22:16:24 -0700

    practices 2 y 3

--- unit 1/practices/practice2.R
+++ unit 1/practices/practice2.R
@@ -36,4 +36,33 @@ which.min(x)
 x <-c(3:5, 11:8, 8 + 0:5)
 x
 unique(x)
+#11.- is used to obtain the mean
+x <-c(3, 5, 11,8, 8 ,5, 9)
+mean(x)
+#12.- a bar graph can be obtained from a matrix
+plot(matrix(data=5, nr=2, nc=2))
 
+#13.- we can save a variable when executing
+my.name <- readline(prompt="Enter name: ")
+
+#14.- delete a variable
+rm(x)
+x
+
+#15.- we can read a .cvs file, we only enter the address of the file inside the parenthesis
+read.csv()
+
+#16.- total variance calculated, identifies the mean of the squared deviations of a random variable
+var(x, y=NULL, na.rm=FALSE)
+
+#17.- Standard deviation, indicates how scattered the data is from the mean
+sd(x, na.rm=FALSE)
+
+#18.- identifies the value of the median with respect to the vector
+median(x)
+
+#19.- we can run sequences
+sequence(4:5)
+
+#20.- we count the number of elements within a vector
+length(x)
