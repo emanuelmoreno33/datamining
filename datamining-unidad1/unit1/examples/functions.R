29e45abe47db94c2115a8da880219dd9722f75bd
IrvingQuirozV <71294033+IrvingQuirozV@users.noreply.github.com>
2020-10-27 17:13:08 -0700

    Exam finished

---
+++ datamining-unidad1/unit1/examples/functions.R
@@ -0,0 +1,21 @@
+?rnorm()
+
+c()
+seq()
+rep()
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+sqrt()
+paste()
+
+#?
+
+# Practice find 20 more functions in R and make an example of it.
